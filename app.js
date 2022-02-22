5a5889df7058a4a19ac7c4cad792231956776ff4
Gabriel <gabrielmeloc@gmail.com>
2022-02-22 02:48:18 -0300

    update appjs and deploy

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
 // discord bot link https://discord.com/api/oauth2/authorize?client_id=945130274927284234&permissions=448824596544&scope=bot
 
-const { Client, Intents, Interaction } = require('discord.js');
+const { Client, Collection , Intents} = require('discord.js');
+const fs = require('fs');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.commands = new Collection ();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles){
+	const command = require(`./commands/${file}`);
+	//Set a new item in the Collection 
+	//With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;	
+
+	const command = client.commands.get(interaction.commandName);
 	
-	if(commandName === 'ping'){
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server'){
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user'){
-		await interaction.reply(`Your Usarname is: ${interaction.user.username}\nYour tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	if(!command) return;
+
+	try {
+		await command.execute(interaction);
+	}catch (error){
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
 	}
 });
 
