818c68a37dc339c1d9e83e3bba58ab6ec069b347
Gabriel <gabrielmeloc@gmail.com>
2022-02-22 02:02:30 -0300

    update interaction code

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('interactionCreate', async Interaction => {
-	if (!Interaction.isCommand()) return;
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
 	const { commandName } = interaction;
 	
 	if(commandName === 'ping'){
 		await interaction.reply('Pong!');
 	} else if (commandName === 'server'){
-		await interaction.reply('Server info');
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	} else if (commandName === 'user'){
-		await interaction.reply('User info');
+		await interaction.reply(`Your Usarname is: ${interaction.user.username}\nYour tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	}
 });
 
-// Login to Discord with your client's token
+// Login to Discord with client token
 client.login(token);
