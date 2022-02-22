aa9f63f4f68ab0a5548e858e82626b1987272326
Gabriel <gabrielmeloc@gmail.com>
2022-02-21 21:42:57 -0300

    .....

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // discord bot link https://discord.com/api/oauth2/authorize?client_id=945130274927284234&permissions=448824596544&scope=bot
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
