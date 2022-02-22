4340889d7b2b3f1c1b5b7b1892ec5fa4767773ae
Gabriel <gabrielmeloc@gmail.com>
2022-02-21 22:57:21 -0300

    basic syntax

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // discord bot link https://discord.com/api/oauth2/authorize?client_id=945130274927284234&permissions=448824596544&scope=bot
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+
+const { Client, Intents, Interaction } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -9,6 +9,20 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+client.on('interactionCreate', async Interaction => {
+	if (!Interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+	
+	if(commandName === 'ping'){
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server'){
+		await interaction.reply('Server info');
+	} else if (commandName === 'user'){
+		await interaction.reply('User info');
+	}
 });
 
 // Login to Discord with your client's token
